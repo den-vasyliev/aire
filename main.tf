757751420a52d58bdda86e7e5a6cff0e2f4ae770
DevGPT-AI <den.vasyliev@gmail.com>
2024-01-27 16:14:12 +0000

    Create terraform code to deploy flux on KinD Kubernetes cluster using the flux terraform module

---
+++ main.tf
@@ -0,0 +1,19 @@
+module "flux" {
+  source = "fluxcd/flux//kubernetes" // This is the Terraform module for Flux
+  version = "0.0.13" // Specify the version of the Flux module
+
+  git_url    = var.git_url
+  git_branch = var.git_branch
+  git_path   = var.git_path
+
+  set = {
+    "name" = "flux"
+    "namespace" = "flux"
+  }
+
+  depends_on = [kind_cluster.this]
+}
+
+resource "kind_cluster" "this" {
+  name = var.cluster_name
+}
